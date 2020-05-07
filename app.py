044ecf292f8bc297fae0e4149e6e29b7b4f83417
Zineddine <zinedddine97@gmail.com>
2020-05-07 18:09:07 +0200

    Add DELETE /schedule/<schedule_id>

--- app.py
+++ app.py
@@ -13,18 +13,22 @@ def hello():
     return "Hello, World!"
 
 
-@app.route("/schedule/<schedule_id>", methods={'PUT'})
-def put_schedule(schedule_id):
+@app.route("/schedule/<schedule_id>", methods={'PUT', 'DELETE'})
+def put_delete_schedule(schedule_id):
     if not ObjectId.is_valid(schedule_id):
         return 'Invalid schedule id ', 400
     old = db.Schedule.find_one({"_id": ObjectId(schedule_id)})
     if old is None:
         return 'Schedule with id ' + schedule_id + ' not found', 404
-    new = request.get_json()
-    old.pop('_id')
-    update = {"$set": new}
-    db.Schedule.update_one(old, update)
-    return 'Schedule updated', 200
+    if request.method == 'PUT':
+        new = request.get_json()
+        old.pop('_id')
+        update = {"$set": new}
+        db.Schedule.update_one(old, update)
+        return 'Schedule updated', 200
+    elif request.method == 'DELETE':
+        db.Schedule.delete_one(old)
+        return 'Schedule deleted', 200
 
 
 if __name__ == '__main__':
