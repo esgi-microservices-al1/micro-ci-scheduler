9c273d2ec4b79f7d4bd87df199ff1b89b362fd16
Allan <amaubert2@myges.fr>
2020-05-08 15:44:35 +0200

    add route /schedule

--- app.py
+++ app.py
@@ -4,9 +4,23 @@ import pymongo
 
 app = Flask(__name__)
 
+#MongoDB will create the collection if it does not exist.
+print(db)
+
+print(db.())
+
+scheduler_collection = db['schedule']
+
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+@app.route("/schedule")
+def update_schedule():
+    schedule = {"name": "John", "address": "Highway 37" }
+    inserted = scheduler_collection.insert_one(schedule)
+    print(inserted)
+    return inserted
 
 
 if __name__ == '__main__':
