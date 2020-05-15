01f23887a3ddcab2cc04aad707ced428c6379eb2
Thomas BRAZE <thomas.braze@opalesystems.com>
2020-05-15 12:22:18 +0200

    add script directory

---
+++ scriptsRabbitMQ/receiver.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import sys
+
+import pika
+
+def receive (ip, port, login, pwd, queue):
+
+    credentials = pika.PlainCredentials(login, pwd)
+
+    connection = pika.BlockingConnection(pika.ConnectionParameters(ip, port, '/', credentials))
+
+    channel = connection.channel()
+
+    channel.queue_declare(queue=queue)
+
+
+    def callback(ch, method, properties, body):
+        print(" [x] Received %r" % body)
+        """
+        run a scheduler form body
+        """
+        return body
+
+
+    channel.basic_consume(queue=queue,
+                      auto_ack=True,
+                      on_message_callback=callback)
+
+    print(' [x] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
--- scriptsRabbitMQ/sender.py
+++ scriptsRabbitMQ/sender.py
@@ -4,15 +4,6 @@ import pika
 
 
 def send(ip, port, login, pwd, queue, body):
-    # ip = #192.168.1.94   // Where is rabbitmq installed
-    # port = #5672           // Default port for AMQP
-
-    # login =  #guest          // Default user
-    # pwd =    #guest          // Default pwd
-
-    # queue =   #hello          // The name of the queue
-
-    # body =         #'Hello World'  // Message to sent to the queue
 
     credentials = pika.PlainCredentials(login, pwd)
     connection = pika.BlockingConnection(pika.ConnectionParameters(ip, port, '/', credentials))
