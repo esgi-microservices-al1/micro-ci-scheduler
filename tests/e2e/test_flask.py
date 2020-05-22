2774f7160aa2cb77c4d8ed4c5b75a59d9ece51f6
Zineddine <zinedddine97@gmail.com>
2020-05-22 19:58:26 +0200

    Add tests for all routes /shedule

--- tests/e2e/test_flask.py
+++ tests/e2e/test_flask.py
@@ -1,4 +1,5 @@
 import json
+import pickle
 from datetime import datetime
 import pytest
 from app import app
@@ -12,7 +13,29 @@ def client():
         yield client
 
 
-class TestAPI():
+testSchedule = dict(
+    name="scheduleTest",
+    project="projectTest",
+    branch="master",
+    interval=dict(unity='DAY', frequency=1),
+    startDate=str(datetime.now())
+)
+updatedSchedule = dict(
+    name="updatedScheduleTest",
+    project="projectTest",
+    branch="master",
+    interval=dict(unity='DAY', frequency=1),
+    startDate=str(datetime.now())
+)
+
+
+class TestAPI:
+    def test_get_root(self):
+        app.testing = True
+        with app.test_client() as client:
+            res = client.get('/')
+            assert res.status_code == 200
+
     # @pytest.fixture
     # def create_app(self):
     #     application = app.create_app()
@@ -21,23 +44,45 @@ class TestAPI():
     #     # application.config['DATABASE'] = tempfile.mkstemp()
     #     application.testing = True
 
-    def test_get_root(self):
-        app.testing = True
-        with app.test_client() as client:
-            res = client.get('/')
-            assert res.status_code == 200
-
     def test_post_get_schedule(self):
         with app.test_client() as client:
-            post = client.post('/schedule', data=json.dumps(dict(
-                name="scheduleTest",
-                project="projectTest",
-                branch="master",
-                interval=dict(unity='DAY', frequency=1),
-                startDate=str(datetime.now())
-            )), follow_redirects=True, headers={'Content-Type': 'application/json'})
-            print(post.data)
+            post = client.post('/schedule', data=json.dumps(testSchedule), follow_redirects=True,
+                               headers={'Content-Type': 'application/json'})
             assert post.status_code == 201
             res = client.get('/schedule/')
             assert res.status_code == 200
             assert b'"name": "scheduleTest' in res.data
+
+    def test_post_delete_schedule(self):
+        with app.test_client() as client:
+            post = client.post('/schedule', data=json.dumps(testSchedule), follow_redirects=True,
+                               headers={'Content-Type': 'application/json'})
+            data = json.loads(post.data)
+            assert post.status_code == 201
+            res = client.delete("/schedule/" + data['id'])
+            assert res.status_code == 200
+
+    def test_post_put_schedule(self):
+        with app.test_client() as client:
+            post = client.post('/schedule', data=json.dumps(testSchedule), follow_redirects=True,
+                               headers={'Content-Type': 'application/json'})
+            data = json.loads(post.data)
+            assert post.status_code == 201
+            res = client.put("/schedule/" + data['id'], data=json.dumps(updatedSchedule), follow_redirects=True,
+                               headers={'Content-Type': 'application/json'})
+            assert res.status_code == 200
+
+    def test_post_get_put_delete(self):
+        with app.test_client() as client:
+            post = client.post('/schedule', data=json.dumps(testSchedule), follow_redirects=True,
+                               headers={'Content-Type': 'application/json'})
+            data = json.loads(post.data)
+            assert post.status_code == 201
+            res = client.get('/schedule/' + data['id'])
+            assert res.status_code == 200
+            assert b'"name": "scheduleTest' in res.data
+            res = client.put("/schedule/" + data['id'], data=json.dumps(updatedSchedule), follow_redirects=True,
+                               headers={'Content-Type': 'application/json'})
+            assert res.status_code == 200
+            res = client.delete("/schedule/" + data['id'])
+            assert res.status_code == 200
