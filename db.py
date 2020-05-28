82ffa74830f52d6935a8bfe9c6cdd287a2cf39cd
Allan <amaubert2@myges.fr>
2020-05-28 19:13:20 +0200

    change db.py

--- db.py
+++ db.py
@@ -17,15 +17,17 @@ app = Flask(__name__)
 #     db = client.testdb
 
 
-if os.environ['TESTING'] == "False":
+if os.getenv('TESTING') is not None and os.environ['TESTING'] == "True":
+    print("USING MEMORY")
+    client = MemoryMongoClient()
+    db = client.schedulerdb
+
+
+else:
     print("USING ALTAS")
     db_user = os.environ['DB_USER']
     db_password = os.environ['DB_PWD']
     db_host = os.environ['DB_HOST']
     db_name = os.environ['DB_NAME']
     client = MongoClient(f'mongodb+srv://{db_user}:{db_password}@{db_host}/{db_name}')
-    db = client.schedulerdb
-else:
-    print("USING MEMORY")
-    client = MemoryMongoClient()
     db = client.schedulerdb
