15a48c2f9aaa4c5911a6ed7edcd857a90ec182b7
Allan <amaubert2@myges.fr>
2020-06-12 20:07:01 +0200

    add CORS Origin Access

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS
 from flask_restplus import Api
 from pymongo_inmemory.downloader import _mkdir_ifnot_exist
 
@@ -9,11 +10,14 @@ from apis.api_communication import namespace as communication_namespace
 
 app = Flask(__name__)
 
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
 api = Api(app, version='1.0', title='Micro-CI-Scheduler API',
           description='a Flask based API for the Micro-CI-Scheduler micro-service')
 
 api.add_namespace(schedule_namespace)
 api.add_namespace(communication_namespace)
+
 
 print(os.environ.get("PYMONGOIM__BIN_FOLDER", _mkdir_ifnot_exist("bin")))
 if __name__ == '__main__':
